51a0ddf4942e70ed81c9ea239411a96273f8636d
OscarRojasPruebas <oscar.rojasroa@gmail.com>
2024-08-06 09:29:40 -0500

    Commit FOR and WHILE

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+//Ejercicio con FOR
+public class Main {
+    public static void main(String[] args) {
+         int suma = 0;
+
+         for (int i = 1; i <= 10; i++) {
+             suma += i;
+         }
+         System.out.println("La sumatoria de los números comprendidos entre 1 y 10 es: " + suma);
+  }
+}
+
+//Ejercicio con WHILE
+/*public class Main {
+    public static void main(String[] args) {
+        int suma=0;
+        int i = 1;
+
+        while (i <= 10) {
+            suma += i;
+            i++;
+        }
+        System.out.println("La sumatoria de los números comprendidos entre 1 y 10 es:  " + suma);
+    }
+}*/
